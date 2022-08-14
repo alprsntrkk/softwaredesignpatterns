dd2262d0b2df25e121a57f66f33284f54d82f962
alprsntrkk <alper.senturk@outlook.com.tr>
2022-08-14 18:00:41 +0300

    Medaitor pattern added.

---
+++ softwaredesignpatterns.consoleapp/Patterns/Behavioral/MediatorPattern/AdminUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace softwaredesignpatterns.consoleapp.Patterns.Behavioral.MediatorPattern
+{
+    public class AdminUser : User
+    {
+        public AdminUser(IUserMediator mediator) : base(mediator)
+        {
+        }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Admin gets message: " + message);
+        }
+
+        public override void Send(string message)
+        {
+            _mediator.Send(message, this);
+        }
+    }
+}
---
+++ softwaredesignpatterns.consoleapp/Patterns/Behavioral/MediatorPattern/IUserMediator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace softwaredesignpatterns.consoleapp.Patterns.Behavioral.MediatorPattern
+{
+    public interface IUserMediator
+    {
+        void Send(string message, User user);
+    }
+}
