b9debd01aca411a1024ec93fc091fc07990b8768
alprsntrkk <alper.senturk@outlook.com.tr>
2022-08-14 18:27:58 +0300

    commandmediatortests

---
+++ softwaredesignpatterns.test/Behavioral/CommandPattern/CommandPatternTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using softwaredesignpatterns.consoleapp.Patterns.Behavioral.CommandPattern;
+using Assert = Xunit.Assert;
+
+namespace softwaredesignpatterns.test.Behavioral.CommandPattern
+{
+    public class CommandPatternTests
+    {
+        [Fact]
+        public void TestCommandPattern()
+        {
+            Invoker invoker = new Invoker();
+            invoker.setCommand(new SayHiCommand("Alper"));
+            invoker.buttonPressed();
+        }
+    }
+}
---
+++ softwaredesignpatterns.test/Behavioral/MediatorPattern/MediatorPatternTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using softwaredesignpatterns.consoleapp.Patterns.Behavioral.MediatorPattern;
+
+namespace softwaredesignpatterns.test.Behavioral.MediatorPattern
+{
+    public class MediatorPatternTest
+    {
+        [Fact]
+        public void TestMediatorPattern()
+        {
+            ConcreteUserMediator concreteUserMediator = new ConcreteUserMediator();
+
+            RegularUser regularUser = new RegularUser(concreteUserMediator);
+            AdminUser adminUser = new AdminUser(concreteUserMediator);
+
+            concreteUserMediator.FirstUser = regularUser;
+            concreteUserMediator.SecondUser = adminUser;
+
+            regularUser.Send("test");
+            adminUser.Send("admin test");
+        }
+    }
+}
