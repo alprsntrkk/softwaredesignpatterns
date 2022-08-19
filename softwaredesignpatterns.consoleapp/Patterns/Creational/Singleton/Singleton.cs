02b69057e13b6410d98c96a0b287b75f7294bb76
Alper SENTURK <alper.senturk@arkas.com>
2022-08-19 14:24:34 +0300

    Singleton implementation added.

---
+++ softwaredesignpatterns.consoleapp/Patterns/Creational/Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace softwaredesignpatterns.consoleapp.Patterns.Creational.Singleton
+{
+    public class Singleton
+    {
+        public Singleton()
+        {
+        }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
